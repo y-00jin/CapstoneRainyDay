4d6a86bbff1b5d20cd9bbb3834d033f35bd6fa28
y-00jin <81798918+y-00jin@users.noreply.github.com>
2022-09-23 18:25:23 +0900

    기본 프로젝트

---
+++ Yoojin_Capstone/src/main/java/kr/inhatc/capstone/YoojinCapstoneApplication.java
@@ -0,0 +1,13 @@
+package kr.inhatc.capstone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoojinCapstoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoojinCapstoneApplication.class, args);
+	}
+
+}
